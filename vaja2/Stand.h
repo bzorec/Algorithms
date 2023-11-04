578c8391d4a6f222f511f5b42f6fa7dfc6660c6c
bzorec <blaz.zorec2002@gmail.com>
2023-11-04 23:17:00 +0100

    vaja2

---
+++ vaja2/Stand.h
@@ -0,0 +1,46 @@
+﻿#include <vector>
+#include <algorithm>
+
+class Stand {
+public:
+    int findMinimum() {
+        return *std::min_element(_weights.begin(), _weights.end());
+    }
+
+    int findMaximum() {
+        return *std::max_element(_weights.begin(), _weights.end());
+    }
+
+    void trim(int x) {
+        _weights.erase(std::remove_if(_weights.begin(), _weights.end(), [x](int val) { return val < x + 1; }),
+                       _weights.end());
+    }
+
+    bool arePairsValid() {
+        for (size_t i = 0; i < _weights.size(); i += 2) {
+            if (i + 1 < _weights.size() && _weights[i] != _weights[i + 1]) {
+                return false;
+            }
+        }
+        for (size_t i = 0; i < _weights.size(); i += 2)
+            if (_weights.size() == 1)
+                return false;
+
+        return true;
+    }
+
+    bool anyWeights() {
+        return !_weights.empty();
+    }
+
+    void addWeight(int number) {
+        _weights.push_back(number);
+    }
+
+    std::vector<int> getWeights() {
+        return _weights;
+    }
+
+private:
+    std::vector<int> _weights;
+};
---
+++ vaja2/main.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include "Stand.h"
+
+bool checkX(Stand *firstStand, Stand *secondStand, int x) {
+    auto first = *firstStand;
+    auto second = *secondStand;
+
+    first.trim(x);
+    second.trim(x);
+
+    bool isValidFirstStand;
+    bool isValidSecondStand;
+
+    if (!first.anyWeights() && !second.anyWeights()) {
+        return true;
+    }
+
+    isValidFirstStand = first.arePairsValid();
+    isValidSecondStand = second.arePairsValid();
+
+    return isValidFirstStand && isValidSecondStand;
+}
+
+int bisect(Stand *firstStand, Stand *secondStand, int a, int b) {
+    while (b - a > 1) {
+        int x = (a + b) / 2;
+
+        bool fa = checkX(firstStand, secondStand, a);
+        bool fb = checkX(firstStand, secondStand, b);
+        bool fx = checkX(firstStand, secondStand, x);
+
+        if (!fa && fb && fx) {
+            b = x;
+        } else if (!fa && !fx && fb) {
+            a = x;
+            firstStand->trim(x);
+            secondStand->trim(x);
+        } else {
+            break;
+        }
+    }
+
+    return b;
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        std::cout << "Incorrect number of arguments!\n";
+        return -1;
+    }
+
+    std::string filePath = argv[1];
+    std::ifstream file(filePath);
+
+    int weightPairCount, currentNumber;
+
+    std::vector<int> weights1, weights2;
+
+    if (!file.is_open()) {
+        std::cout << "Error opening the input file.\n";
+        return -1;
+    }
+    auto firstStand = new Stand();
+    auto secondStand = new Stand();
+
+    file >> weightPairCount;
+
+    for (int i = 0; i < weightPairCount * 2; i++) {
+        file >> currentNumber;
+
+        if (i < weightPairCount) {
+            firstStand->addWeight(currentNumber);
+        } else {
+            secondStand->addWeight(currentNumber);
+        }
+    }
+
+    if (firstStand->anyWeights() && secondStand->anyWeights()) {
+        int min = std::min(firstStand->findMinimum(), secondStand->findMinimum());
+        int max = std::max(firstStand->findMaximum(), secondStand->findMaximum());
+
+        if (checkX(firstStand, secondStand, min)) {
+            std::cout << "0";
+            return 0;
+        }
+
+        auto bisectResult = bisect(firstStand, secondStand, min, max);
+        std::cout << bisectResult;
+
+        return 0;
+    }
+
+    file.close();
+    return 0;
+}
