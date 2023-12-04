432f344c378bfc6b8ada00cea480e0a25ea23ec2
bzorec <blaz.zorec@student.um.si>
2023-12-04 13:50:34 +0100

    fixes

--- vaja4/main.cpp
+++ vaja4/main.cpp
@@ -1,61 +1,68 @@
 #include <iostream>
-#include <fstream>
 #include <vector>
-#include <limits>
+#include <climits>
+#include <algorithm>
 
 using namespace std;
 
-int main(const int argc, char* argv[]) {
-    if (argc != 2) {
-        cerr << "Uporaba: " << argv[0] << " <vhodna_datoteka.txt>" << endl;
-        return 1;
-    }
+struct Island {
+    int x, y, t; // x and y coordinates, time to explore
+};
 
-    ifstream file(argv[1]);
-    if (!file) {
-        cerr << "Napaka pri odpiranju datoteke." << endl;
-        return 1;
-    }
+class Solution {
+public:
+    vector<long long> exploreIslands(int N, int M, int K, vector<Island>& islands) {
+        vector<vector<long long>> dp(N + 1, vector<long long>(M + 1, LLONG_MAX));
+        vector<long long> result;
 
-    int N, M, K;
-    file >> N >> M >> K;
+        dp[1][1] = 0;
 
-    vector<vector<int>> islandData(K, vector<int>(3, 0));
-    for (int i = 0; i < K; ++i) {
-        file >> islandData[i][0] >> islandData[i][1] >> islandData[i][2];
-    }
+        for (int i = 1; i <= N; ++i) {
+            vector<long long> A(M + 1, LLONG_MAX);
+            vector<long long> RESULT(M + 1, LLONG_MAX);
+            A[0] = 0;
 
-    vector<int> times(N + 1, 0); // Časi potrebnih za dosego otokov (indeksiranje od 1)
-    vector<int> RESULT(K + 1, numeric_limits<int>::max()); // Polje za hranjenje minimalnega časa za raziskovanje
-    vector<vector<int>> A(K + 1, vector<int>(M + 1, numeric_limits<int>::max()));
-    // Tabela stanj za hranjenje minimalnega časa za raziskovanje k otokov + čas premikov v nove vrstice
+            for (int j = 1; j <= M; ++j) {
+                A[j] = min(A[j - 1] + 3, RESULT[j - 1] + 3);
+                RESULT[j] = min(RESULT[j], A[j] + islands[i - 1].t);
+            }
 
-    // Branje časov potrebnih za dosego otokov
-    for (int i = 0; i < K; ++i) {
-        times[islandData[i][0]] = islandData[i][2];
-    }
+            for (int j = M - 1; j >= 1; --j) {
+                A[j] = min(A[j], RESULT[j + 1] + 3);
+                RESULT[j] = min(RESULT[j], A[j] + islands[i - 1].t);
+            }
 
-    // Dinamično programiranje za premikanje med otoki v vrstici
-    for (int i = 1; i < N; ++i) {
-        for (int j = 0; j <= N; ++j) {
-            int moveCost = 0;
-            for (int k = j + 1; k <= N; ++k) {
-                moveCost += times[k];
-                A[k][1] = min(A[k][1], A[j][0] + moveCost);
+            for (int j = 1; j <= M; ++j) {
+                dp[i][j] = RESULT[j];
             }
         }
-    }
 
-    // Dinamično programiranje za celotno nalogo
-    for (int k = 1; k <= K; ++k) {
-        for (int i = k; i <= N; ++i) {
-            RESULT[k] = min(RESULT[k], A[i][1]);
+        for (int i = 0; i < K; ++i) {
+            result.push_back(dp[N][islands[i].y]);
         }
-    }
 
-    // Izpis rezultatov
-    for (int k = 1; k <= K; ++k) {
-        cout << RESULT[k] << " ";
+        return result;
+    }
+};
+
+int main() {
+    Solution solution;
+
+    // Input data
+    int N = 4, M = 4, K = 5;
+    vector<Island> islands = {
+        {1, 3, 1},
+        {1, 2, 9},
+        {2, 3, 5},
+        {3, 2, 6},
+        {4, 3, 2}
+    };
+
+    vector<long long> result = solution.exploreIslands(N, M, K, islands);
+
+    // Output the result
+    for (long long res : result) {
+        cout << res << " ";
     }
 
     return 0;
