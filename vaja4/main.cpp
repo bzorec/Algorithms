6d51771e0216b694dd135a81510c62f3480cde45
bzorec <blaz.zorec@student.um.si>
2023-12-06 12:39:31 +0100

    fixes

--- vaja4/main.cpp
+++ vaja4/main.cpp
@@ -1,99 +1,103 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
-#include <climits>
 #include <algorithm>
 
 using namespace std;
 
 struct Island {
-    int x, y, t; // x and y coordinates, time to explore
+    int x;
+    int y;
+    long long t;
 };
-// class Solution {
-// public:
-//     int minPathSum(vector<vector<int>>& grid) {
-//         //Fast I/O in C++
-//         ios_base::sync_with_stdio(false);
-//         cin.tie(NULL);
-//         
-//         int rows = grid.size();
-//         if(rows==0)
-//             return 0;
-//         int cols = grid[0].size();
-//         vector<vector<int>> dp(rows,vector<int>(cols,0));
-//         int i,j;
-//         
-//         dp[0][0] = grid[0][0];  //1st element is starting point
-//         //Fill 1st row
-//         for(i=1;i<cols;++i)
-//             dp[0][i] = dp[0][i-1] + grid[0][i];
-//         
-//         //Fill 1st Col
-//         for(i=1;i<rows;++i)
-//             dp[i][0] = dp[i-1][0] + grid[i][0];
-//         
-//         //Now fill the rest of the cell
-//         for(i=1;i<rows;++i)
-//         {
-//             for(j=1;j<cols;++j)
-//                 dp[i][j] = grid[i][j] + min(dp[i-1][j],dp[i][j-1]);
-//         }
-//         return dp[rows-1][cols-1];
-//     }
-// };
-class Solution {
-public:
-    vector<long long> exploreIslands(int N, int M, int K, vector<Island>& islands) {
-        vector<vector<long long>> dp(N + 1, vector<long long>(M + 1, LLONG_MAX));
-        vector<long long> result;
 
-        dp[1][1] = 0;
+vector<long long> minSumPath(const vector<Island>&islands, int n, int m, const int k) {
+    // Create a 2D table to store intermediate results
+    vector<vector<long long>> dp(n + 1, vector<long long>(k + 1, INT_MAX));
 
-        for (int i = 1; i <= N; ++i) {
-            vector<long long> A(M + 1, LLONG_MAX);
-            vector<long long> RESULT(M + 1, LLONG_MAX);
-            A[0] = 0;
 
-            for (int j = 1; j <= M; ++j) {
-                A[j] = min(A[j - 1] + 3, RESULT[j - 1] + 3);
-                RESULT[j] = min(RESULT[j], A[j] + islands[i - 1].t);
-            }
-
-            for (int j = M - 1; j >= 1; --j) {
-                A[j] = min(A[j], RESULT[j + 1] + 3);
-                RESULT[j] = min(RESULT[j], A[j] + islands[i - 1].t);
-            }
-
-            for (int j = 1; j <= M; ++j) {
-                dp[i][j] = RESULT[j];
-            }
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < k; j++) {
+            dp[i][j] = INT_MAX;
         }
-
-        for (int i = 0; i < K; ++i) {
-            result.push_back(dp[N][islands[i].y]);
-        }
-
-        return result;
     }
-};
+
+    dp[0][0] = 0;
+
+    // TODO fix the movment checks not calculation the moves correctly
+    // Fill the table using tabulation
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 0; j <= k; ++j) {
+            // Option 1: Enter a new row in column y=1
+            auto val1 = dp[i][j];
+            auto val2 = dp[i - 1][j] + 1 + islands[i - 1].t;
+            dp[i][j] = min(val1, val2);
+
+            // Option 2: Enter the next row again in column y=1
+            if (j > 0) {
+                auto val1 = dp[i][j];
+                auto val2 = dp[i][j - 1] + 1 + islands[i - 1].y - islands[i - 2].y + islands[i - 1].t;
+                dp[i][j] = min(val1, val2);
+            }
+
+            // Option 3: Enter the next row in the column y=M
+            if (j > 0) {
+                auto val1 = dp[i][j];
+                auto val2 = dp[i - 1][j - 1] + 1 + islands[i - 2].y + islands[i - 1].t;
+                dp[i][j] = min(val1, val2);
+            }
+
+            // Option 4: Enter the new row in the column y=M
+            val1 = dp[i][j];
+            val2 = dp[i - 1][j] + 1 + islands[i - 2].y + islands[i - 1].t;
+            dp[i][j] = min(val1, val2);
+        }
+    }
+
+    // Extract the result from the last row of the table
+    vector<long long> resultPath(k + 1);
+    for (int i = 0; i < k; i++) {
+        resultPath[i] = INT_MAX;
+    }
+
+    // TODO fix the result how to put into the new array
+    for (int j = 0; j <= k; ++j) {
+        auto result = (dp[n][j]);
+        resultPath[j] = result;
+    }
+
+    return resultPath;
+}
 
 int main() {
-    Solution solution;
+    const string filename = "test.txt";
 
-    // Input data
-    int N = 4, M = 4, K = 5;
-    vector<Island> islands = {
-        {1, 3, 1},
-        {1, 2, 9},
-        {2, 3, 5},
-        {3, 2, 6},
-        {4, 3, 2}
-    };
+    ifstream inputFile(filename);
+    if (!inputFile.is_open()) {
+        cerr << "Error opening the file!" << endl;
+        return 1;
+    }
 
-    vector<long long> result = solution.exploreIslands(N, M, K, islands);
+    // Example usage
+    int N, M, K;
+    inputFile >> N >> M >> K;
 
-    // Output the result
-    for (long long res : result) {
-        cout << res << " ";
+    // Input island positions and exploration times
+    vector<Island> islands(K);
+    for (int i = 0; i < K; ++i) {
+        inputFile >> islands[i].x >> islands[i].y >> islands[i].t;
+    }
+
+    // Check if vector islands has enough elements
+    if (islands.size() < K) {
+        cerr << "Error: Insufficient elements in the islands vector." << endl;
+        return 1;
+    }
+
+    // Calculate the minimum sum path using dynamic programming
+    // Output the result path
+    for (const vector<long long> resultPath = minSumPath(islands, N, M, K); const long long time: resultPath) {
+        cout << time << " ";
     }
 
     return 0;
