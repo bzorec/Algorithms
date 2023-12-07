cf47b4d49b7649fc975436fc822f735ba81a0e6c
bzorec <blaz.zorec@student.um.si>
2023-12-07 09:26:46 +0100

    fixes

--- vaja4/vaja4/vaja4/vaja4.cpp
+++ vaja4/vaja4/vaja4/vaja4.cpp
@@ -5,87 +5,110 @@
 
 using namespace std;
 
-struct Island {
-    int x;
-    int y;
-    long long t;
+struct island
+{
+	int x;
+	int y;
+	long long t;
 };
 
-vector<long long> minSumPath(const vector<Island>& islands, int n, int m, const int k) {
-    // Create a 2D table to store intermediate results
-    vector<vector<long long>> dp(n + 1, vector<long long>(k + 1, INT_MAX));
-
-    dp[0][0] = 0;
-    
-    // Fill the table using tabulation
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 0; j <= k; ++j) {
-            // Option 1: Move forward
-            auto val1 = dp[i][j];
-            auto val2 = (i == 1) ? islands[i - 1].y - i + islands[i - 1].t : dp[i - 1][j] + 1 + islands[i - 1].t;
-            dp[i][j] = min(val1, val2);
-
-            // Option 2: Move backward
-            if (j > 0) {
-                auto val1 = dp[i][j];
-                auto val2 = dp[i][j - 1] + (abs(islands[i].y - islands[i - 1].y) + 1) + islands[i - 1].t;
-                dp[i][j] = min(val1, val2);
-            }
-
-            // Option 3: Move upward if in the first or last column
-            if (j > 0 && (i == 1 || i == n)) {
-                auto val1 = dp[i][j];
-                auto val2 = dp[i - 1][j - 1] + (abs(islands[i - 1].y - islands[i].y) + 1) + islands[i - 1].t;
-                dp[i][j] = min(val1, val2);
-            }
-        }
-    }
-
-
-    // Extract the result from the last row of the table
-    vector<long long> resultPath(k);
-
-    for (int j = 0; j < k; ++j) {
-        long long minTime = INT_MAX;
-        for (int i = 0; i <= n; ++i) {
-            minTime = min(minTime, dp[j][i]);
-        }
-        resultPath[j] = minTime;
-    }
-
-    return resultPath;
+int abs_diff(const int x, const int y)
+{
+	return (x > y) ? x - y : y - x;
 }
 
-int main(int argc, char* argv[]) {
+vector<long long> min_sum_path(const vector<island>& islands, const int n, const int m, const int k)
+{
+	// Create a 2D table to store intermediate results
+	vector dp(k + 1, vector<long long>(k + 1, LLONG_MAX - 1));
 
-    ifstream inputFile(argv[1]);
-    if (!inputFile.is_open()) {
-        cerr << "Error opening the file!" << endl;
-        return 1;
-    }
+	dp[0][0] = 0;
 
-    // Example usage
-    int N, M, K;
-    inputFile >> N >> M >> K;
+	// Fill the table using tabulation
+	for (int i = 1; i <= n; ++i)
+	{
+		for (int j = 0; j <= k; ++j)
+		{
+			// Option 1: Move forward
+			auto left1 = dp[i][j];
+			auto right1 = dp[i - 1][j] + (abs_diff(islands[i].y, islands[i - 1].y) + 1) + islands[i - 1].t;
+			dp[i][j] = min(left1, right1);
 
-    // Input island positions and exploration times
-    vector<Island> islands(K);
-    for (int i = 0; i < K; ++i) {
-        inputFile >> islands[i].x >> islands[i].y >> islands[i].t;
-    }
+			// Option 2: Move backward
+			if (j > 0)
+			{
+				auto left2 = dp[i][j];
+				auto right2 = dp[i][j - 1] + (abs_diff(islands[i].y, islands[i - 1].y) + 1) + islands[i - 1].t;
+				dp[i][j] = min(left2, right2);
+			}
 
-    // Check if vector islands has enough elements
-    if (islands.size() < K) {
-        cerr << "Error: Insufficient elements in the islands vector." << endl;
-        return 1;
-    }
+			// Option 3: Move upward if in the first or last column
+			if (j > 0 && (i == 1 || i == n))
+			{
+				auto left3 = dp[i][j];
+				auto right3 = dp[i - 1][j - 1] + (abs_diff(islands[i - 1].y, islands[i].y) + 1) + islands[i - 1].t;
+				dp[i][j] = min(left3, right3);
+			}
 
-    // Calculate the minimum sum path using tabulation
-    vector<long long> resultPath = minSumPath(islands,N,M, K);
+			// Initialize the first column of the dp table to 0
+			for (int l = 1; l <= k; ++l)
+			{
+				dp[0][l] = 0;
+			}
+		}
+	}
 
-    // Output the result path
-    for (int time : resultPath) {
-        cout << time << " ";
-    }
-    return 0;
+	// Extract the result path
+	vector<long long> result_path(k + 1);
+
+	for (int j = 0; j < k; ++j)
+	{
+		long long min_time = INT_MAX;
+		for (int i = 0; i <= n; ++i)
+		{
+			min_time = min(min_time, dp[j][i]);
+		}
+		result_path[j] = min_time;
+	}
+
+	return result_path;
+}
+
+
+int main(int argc, char* argv[])
+{
+	ifstream input_file(argv[1]);
+	if (!input_file.is_open())
+	{
+		cerr << "Error opening the file!" << '\n';
+		return 1;
+	}
+
+	// Example usage
+	int n, m, k;
+	input_file >> n >> m >> k;
+
+	// Input island positions and exploration times
+	vector<island> islands(k);
+	for (int i = 0; i < k; ++i)
+	{
+		input_file >> islands[i].x >> islands[i].y >> islands[i].t;
+	}
+
+	// Check if vector islands has enough elements
+	if (islands.size() < k)
+	{
+		cerr << "Error: Insufficient elements in the islands vector." << '\n';
+		return 1;
+	}
+
+	// Calculate the minimum sum path using tabulation
+
+	// Output the result path
+	for (const vector<long long> result_path = min_sum_path(islands, n, m, k); const long long time : result_path)
+	{
+		cout << time << " ";
+	}
+
+	return 0;
 }
